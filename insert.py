d6b239533dac06e317619a6c7b1a7364989cfa02
sofisop <sofisop2001@gmail.com>
2020-05-17 02:51:05 +0000

    fixed commit

--- insert.py
+++ insert.py
@@ -6,7 +6,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("db", default=":memory:", help="The file path of the database to be edited")
-parser.add_argument("-r", "--roster", help="Inserts names from ROSTER into db. ROSTER should be a plain-text file with one name on each line.")
+parser.add_argument("-r", "--roster")
 args = parser.parse_args()
 
 # ===== END CLI BLOCK ===== #
@@ -18,10 +18,6 @@ if args.roster:
     with open(args.roster) as f:
         for record in f:
             name = record.strip()
-            name = name.replace('(', '')
-            name = name.replace(')', '')
-            name = name.replace(';', '')
-            name = name.replace("'", '')
             cur.executescript(f"INSERT INTO students (name) values ('{name}')")
             conn.commit()
         conn.close()
@@ -29,12 +25,6 @@ if args.roster:
 else:
     while True:
         name = input("Type your first and last name and hit enter to register. Hit Ctrl+C to quit.\n> ")
-        name = record.strip()
-        name = name.replace('(', '')
-        name = name.replace(')', '')
-        name = name.replace(';', '')
-        name = name.replace("'", '')
         cur.execute(f"INSERT INTO students (name) VALUES ('{name}');")
         conn.commit()
-        conn.close()
 
